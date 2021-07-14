a6d1cfb9c09d793393df83df5a5647cd49b66c76
TheSoliderror <83806741+TheSoliderror@users.noreply.github.com>
2021-07-14 14:14:49 -0600

    Add files via upload

---
+++ Map_Chooser_AW/__init__.lua
@@ -0,0 +1,36 @@
+game:executecommand("say AW Simple Zombie Map Chooser Loaded")
+
+
+local onPlayerSay = function (player, msg)
+    msg = string.lower(msg);
+
+    if msg == "!rs" or msg == "!r" then
+		game:executecommand("fast_restart")
+    end
+	
+	if msg == "!outbreak" or msg == "!out" then
+		game:executecommand("map mp_zombie_lab")
+    end
+	
+	if msg == "!infection" or msg == "!infect" then
+		game:executecommand("map mp_zombie_brg")
+    end
+	
+	if msg == "!carrier" or msg == "!car" then
+		game:executecommand("map mp_zombie_ark")
+    end
+	
+	if msg == "!descent" or msg == "!des" then
+		game:executecommand("map mp_zombie_h20")
+    end
+
+	
+end
+
+local onPlayerConnected = function (player)
+	local saylistener = player:onnotify("say", function(msg) onPlayerSay(player, msg) end);
+        
+end
+
+
+level:onnotify("connected", onPlayerConnected);
